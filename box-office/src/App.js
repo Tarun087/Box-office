72204f13e93f320e69d06ce1bd7ddd856c3e8755
Tarun <tamangtarun8@gmail.com>
2022-08-02 19:05:56 +0530

    Created and linked home and start pages

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is Home Page
-      </Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <div>Error 404!...</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
