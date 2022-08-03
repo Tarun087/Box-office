da250f5d9a9649e763e2fd294b7a3031910cc37e
Tarun <tamangtarun8@gmail.com>
2022-08-03 23:17:21 +0530

    added loading and error messages

--- box-office/src/Pages/Show.jsx
+++ box-office/src/Pages/Show.jsx
@@ -4,13 +4,41 @@ import { getApi } from '../misc/config';
 
 const Show = () => {
   const { id } = useParams();
+
   const [show, setShow] = useState(null);
+  const [isloading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    getApi(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(Results => {
-      setShow(Results);
-    });
+    let isMounted = true;
+
+    getApi(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(Results => {
+        if (isMounted) {
+          setShow(Results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
   console.log('show', show);
+
+  if (isloading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error Occured:{error} </div>;
+  }
 
   return <div>This is a Show Page</div>;
 };
